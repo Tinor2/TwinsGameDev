74e0994ddce3cf8b19853f50acf30f1d73d9f833
Tinor2 <ironitbhandari@gmail.com>
2023-08-16 15:03:31 +1000

    Tweaked a few settings
    Fixed the follow for the attack area, and cleaned up the attack area code a little bit

--- The Knight and the Tax-Evading Wizard/Assets/Scripts/AttackArea.cs
+++ The Knight and the Tax-Evading Wizard/Assets/Scripts/AttackArea.cs
@@ -1,8 +1,3 @@
-
-using System.Data.SqlTypes;
-using System.Numerics;
-using Unity.VisualScripting;
-using UnityEditor.Rendering;
 using UnityEngine;
 
 //This script manages the collsion detection created by Knight's sword
@@ -21,7 +16,7 @@ public class AttackArea : MonoBehaviour
 
     void OnDrawGizmosSelected()
     {
-        if (attackPoint == null || !playerCombat.attacking) return;
+        if (attackPoint == null) return;
         Gizmos.DrawWireSphere(attackPoint.position, r);
     }
     void Start(){
@@ -30,20 +25,20 @@ public class AttackArea : MonoBehaviour
     void Update()
     {        
         Collider2D[] enemiesHit = Physics2D.OverlapCircleAll(gameObject.transform.position, r,enemies);
-        CheckDone = false;               
+        if (enemiesHit == null) return;               
         foreach(Collider2D enemy in enemiesHit){
-             //Store that script into a variable
+            //Store that script into a variable
             Health health = enemy.gameObject.GetComponent<Health>();
             //Deal damage to the specific health script
             health.Damage(damage);
             Debug.Log(damage.ToString());
             //If this script is on a bullet then
             if (gameObject.tag == "Bullet") {
-                //Disable the bullet
+            //Disable the bullet
                 gameObject.SetActive(false);
-                }
             }
-        CheckDone = true;
+            }
+
     }      
     
         
--- The Knight and the Tax-Evading Wizard/Assets/Scripts/Health.cs
+++ The Knight and the Tax-Evading Wizard/Assets/Scripts/Health.cs
@@ -1,6 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using TarodevController;
 using UnityEngine;
 
 //The heak=lth system for an enemy
