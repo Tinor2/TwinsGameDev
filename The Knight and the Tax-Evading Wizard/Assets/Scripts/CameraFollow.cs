7266816d6793c25cd14923f3cf92fe19961093a0
Tinor2 <ironitbhandari@gmail.com>
2023-11-21 22:57:55 +1100

    Fixed some of the bugs with Y chanelling
    Tried (and failed) to fix offset, so i removed it for now, fixed the dampening issue, and found a massive issue, concerning the way the channel threshold works. Have not resolved these issues

--- The Knight and the Tax-Evading Wizard/Assets/Scripts/CameraFollow.cs
+++ The Knight and the Tax-Evading Wizard/Assets/Scripts/CameraFollow.cs
@@ -38,31 +38,35 @@ public class CameraFollow : MonoBehaviour
     void Update()
     {
       // Y channeling
+      
       changeY = p.transform.position.y - cameraYSnippet;
-      if(changeY >= 0){ //if net y movement is positive
-        if (changeY >= channelThresh){ 
-          targetY = transform.position.y + channelThresh/2;
-          cameraYSnippet = transform.position.y;
+      if (changeY != 0){
+        if(changeY > 0){ //if net y movement is positive
+          if (changeY >= channelThresh){ 
+            targetY = Mathf.Abs(transform.position.y + channelThresh/3);
+            cameraYSnippet = transform.position.y;
+          }
         }
+        if(changeY < 0){
+          if (changeY <= channelThresh *1 ){ //if net y movement is negative
+            targetY = Mathf.Abs(transform.position.y - channelThresh/3) * -1;
+            cameraYSnippet = transform.position.y;
+            Debug.Log("Negative Change: "+ targetY);
+          }
+        } // Returns targetY value, where the camera should move next
       }
-      if(changeY <= 0){
-        if (changeY <= channelThresh *1 ){ //if net y movement is negative
-          targetY = transform.position.y - channelThresh/2;
-          cameraYSnippet = transform.position.y;
-        }
-        
-      } // Returns targetY value, where the camera should move next
-      changeY = 0;
       cameraComponent.orthographicSize = cameraZoom;
-      Vector3 movemposition = p.transform.position + offset; 
+      Vector3 movemposition = p.transform.position; 
       float newX = Mathf.SmoothDamp(transform.position.x, movemposition.x, ref zeroV.x, DampingX, speedClamp);
-      float newY = Mathf.SmoothDamp(transform.position.y, targetY + offset.y, ref zeroV.y, DampingY, speedClamp);
-      transform.position = new Vector3(newX, newY, offset.z);
+      float newY = Mathf.SmoothDamp(transform.position.y, targetY, ref zeroV.y, DampingY, speedClamp);
+      transform.position = new Vector3(newX, newY, -10f);
       if (pC.facingRight){
         offset.x = Mathf.SmoothDamp(offset.x, lookClamp, ref zeroF,lookTime);
       }  
      else{
         offset.x = Mathf.SmoothDamp(offset.x, lookClamp * -1, ref zeroF,lookTime);
       }
+      
     }
+
 }
