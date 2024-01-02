a6d61f9bd4eca0d7dbaf38528b0e7aceb46df1ba
Tinor2 <ironitbhandari@gmail.com>
2024-01-02 20:11:20 +1100

    Setup the starting variables
    right now there should be a straight line at the desired location, with a certain number of points along the line

--- The Knight and the Tax-Evading Wizard/Assets/GraphingFunctions.cs
+++ The Knight and the Tax-Evading Wizard/Assets/GraphingFunctions.cs
@@ -1,19 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GraphingFunctions : MonoBehaviour
 {
-    [SerializeField] int Resolution;
+    [SerializeField] int resolution;
+    [SerializeField] int pointSpacing;
     [SerializeField] LineRenderer lr;
+
+    public Vector3 focalPoint;
+    public float focalWidth;
 
     // Start is called before the first frame update
     void Start()
     {
-        lr.positionCount = Resolution;
-        for (int i = 0; i < Resolution; i++)
+        lr.positionCount = resolution;
+
+        for (int i = 0; i < resolution; i++)
         {
-            }
+            float nextPos = focalPoint.x - focalWidth + pointSpacing*i;
+            lr.SetPosition(i, new Vector3(nextPos, gameObject.transform.position.y, gameObject.transform.position.z));
+        }
     }
 
     // Update is called once per frame
