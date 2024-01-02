6aa3f188d19c8232b85875a0c58ef05e4bd1a56a
Tinor2 <ironitbhandari@gmail.com>
2024-01-02 20:01:02 +1100

    Remoed a random error oops
    i m d u m b

--- The Knight and the Tax-Evading Wizard/Assets/GraphingFunctions.cs
+++ The Knight and the Tax-Evading Wizard/Assets/GraphingFunctions.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class GraphingFunctions : MonoBehaviour
 {
+    [SerializeField] int Resolution;
+    [SerializeField] LineRenderer lr;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        lr.positionCount = Resolution;
+        for (int i = 0; i < Resolution; i++)
+        {
+            }
     }
 
     // Update is called once per frame
